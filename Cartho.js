b7a7e16bd6367471e9beaea724baed32bc45e642
Diarnou FAYE <diarnou.faye@univ-thies.sn>
2023-10-25 08:07:24 +0000

    Deuxième version de la plateforme

--- Cartho.js
+++
@@ -1,7 +0,0 @@
-// map.js
-// Exemple de script pour afficher une carte interactive (utilisant Leaflet)
-var mymap = L.map('map').setView([13.41, -15.64], 7);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
-
-var marker = L.marker([13.41, -15.64]).addTo(mymap);
-marker.bindPopup("Forêt sénégalaise");
--- TB.js
+++ TB.js
@@ -1,9 +1,58 @@
-// script.js
-// Exemple de fonction pour afficher des données dans la section principale
-function afficherDonnees() {
-    const mainElement = document.querySelector('main');
-    mainElement.innerHTML = '<h2>Données Analytiques</h2><p>Voici quelques données analytiques à afficher.</p>';
-}
+<!-- Graphique à barres -->
+<canvas id="barChart" width="400" height="200"></canvas>
 
-// Appeler la fonction lors du chargement de la page
-window.addEventListener('load', afficherDonnees);
+<!-- Données importantes -->
+<div>
+    <h2>Données importantes</h2>
+    <ul>
+        <li>Donnée 1 : Valeur 1</li>
+        <li>Donnée 2 : Valeur 2</li>
+        <li>Donnée 3 : Valeur 3</li>
+    </ul>
+</div>
+
+<!-- Autres éléments du tableau de bord -->
+<div>
+    <!-- Ajoutez ici d'autres éléments, graphiques, tableaux, etc. -->
+</div>
+
+<!-- Script JavaScript pour initialiser le graphique à barres -->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+    // Données pour le graphique à barres (exemple)
+    var data = {
+        labels: ["Janvier", "Février", "Mars", "Avril", "Mai"],
+        datasets: [{
+            label: "Taux de croissance du Carbonne",
+            data: [12, 19, 3, 5, 2],
+            backgroundColor: [
+                'rgba(255,69, 10, 9.2)',
+                'rgba(0, 0, 255, 3.5)',
+                'rgba(255, 206, 0, 3.2)',
+                'rgba(75, 192, 192, 3.2)',
+                'rgba(153, 0, 255, 3.2)'
+            ],
+            borderColor: [
+                'rgba(0, 0, 0, 1)',
+                'rgba(0, 0, 0, 1)',
+                'rgba(0, 0, 0, 1)',
+                'rgba(0, 0, 0, 1)',
+                'rgba(0, 0, 0, 1)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    // Options du graphique à barres
+    var options = {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Créer un graphique à barres
+    var ctx = document.getElementById('barChart').getContext('2d');
+    var myBarChart = new Chart(ctx, {type: 'bar', data: data, options: options});
+</script>
